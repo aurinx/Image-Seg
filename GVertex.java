10b31800ab001b22776de0b87734ef951547dfdf
AurinC <AurinC@aurins-air-2.mtw.jhu.edu>
2016-04-28 19:33:35 -0400

    check

---
+++ GVertex.java
@@ -0,0 +1,96 @@
+/** Class to represent a vertex (in a graph).
+ * @param <VT> vertex generic
+ */
+public class GVertex<VT> implements Comparable<GVertex> {
+
+    /* Note that the nextID variable had to be moved to the graph class. */
+
+    /** GVertex unique ID number. */
+    private int num;
+
+    /** Data stored in the vertex. */
+    private VT data;
+    /** Unique number for each vertex.*/
+    private int uniqueid;
+    /** Create a new vertex.
+     *  @param d the data to store in the node
+     *  @param id the unique id of the node
+     */
+    public GVertex(VT d, int id) {
+        this.data = d;
+        this.num = id;
+        this.uniqueid = -1;
+    }
+    /**
+     * Create a new vertex with unique id.
+     * @param d data to store in the node
+     * @param id is the id of the vertex
+     * @param auniqueid graph's unique id for the vertex
+     */
+    public GVertex(VT d, int id, int auniqueid) {
+        this.data = d;
+        this.num = id;
+        this.uniqueid = auniqueid;
+    }
+
+    /** Get the id of this vertex.
+     *  @return the id
+     */
+    public int id() {
+        return this.num;
+    }
+    /**
+     * Get data of the vertex.
+     * @return the data
+     */
+    public VT data() {
+        return this.data;
+    }
+    /**
+     * Get unique ID of vertex.
+     * @return the unique id
+     */
+    public int uniqueid() {
+        return this.uniqueid;
+    }
+    /**
+     * Change the unique id of a certain numbered vertex.
+     * @param temp number of the id
+     */
+    public void changeunique(int temp) {
+        this.uniqueid = temp;
+    }
+    /** Get a string representation of the vertex.
+     *  @return the string 
+     */
+    public String toString() {
+        return this.num + "";
+    }
+
+    /** Check if two vertices are the same based on ID.
+     *  @param other the vertex to compare to this
+     *  @return true if the same, false otherwise
+     */
+    public boolean equals(Object other) {
+        if (other instanceof GVertex) {
+            GVertex v = (GVertex) other;
+            return this.num == v.num;  // want these to be unique
+        }
+        return false;
+    }
+
+    /** Get the hashcode of a vertex based on its ID.
+     *  @return the hashcode
+     */
+    public int hashCode() {
+        return (new Integer(this.num)).hashCode();
+    }
+
+    /** Compare two vertices based on their IDs.
+     *  @param other the vertex to compare to this
+     *  @return negative if this < other, 0 if equal, positive if this > other
+     */
+    public int compareTo(GVertex other) {
+        return this.num - other.num;
+    }
+}
---
+++ Graph.java
@@ -0,0 +1,106 @@
+/*
+ * Dimitri Nikitopoulos: dnikito1
+ * Aurin Chakravarty: achakr16
+ * Joo Sung Kim: jkim469
+ * Data Structures 600.226.02
+ * Dr. Selinski & Dr. Hager 
+ * Project 4
+ */import java.util.List;
+/**
+ * Graph interface.
+ *
+ */
+public interface Graph {
+
+    /** Get the number of edges.
+     * @return int number of edges
+     */
+    int numEdges();
+
+    /** Get the number of vertices. 
+     * @return int number of vertices
+     */
+    int numVerts();
+
+    /** Get the next ID to use in making a vertex.
+     * @return int the ID in the sequence
+     */
+    int nextID();
+
+    /** Create and add a vertex to the graph.
+     *  @param d the data to store in the vertex
+     *  @return true if successful, false otherwise
+     */
+    boolean addVertex(Object d);
+
+    /** Add a vertex if it doesn't exist yet. 
+     * @param v vertex to add
+     * @return true if vertex was added
+     */
+    boolean addVertex(Vertex v);
+
+    /** Add an edge, may also add the incident vertices.
+     * @param e edge to add
+     * @return true if edge was added
+     */
+    boolean addEdge(Edge e);
+
+    /** Add a (directed) edge, may also add the incident vertices.
+     * @param v first vertex
+     * @param u second vertex
+     * @return add a directed edge
+     */
+    boolean addEdge(Vertex v, Vertex u);
+
+    /** Remove a (directed) edge if there. 
+     * @param v first vertex to look at
+     * @param u second vertex to look at
+     * @return whether edge was deleted
+     */
+    boolean deleteEdge(Vertex v, Vertex u);
+
+    /** Return true if there is an edge between v and u.
+     * @param v first vertex to check
+     * @param u second vertex to check 
+     * @return true if the vertices are adjacent
+     */
+    boolean areAdjacent(Vertex v, Vertex u);
+
+    /** Return a list of all the neighbors of vertex v.
+     * @param v vertex to find list of neighbors for
+     * @return The list of neighbors from v
+     */
+    List<Vertex> neighbors(Vertex v);
+
+    /** Return the number of edges incident to v. 
+     * @param v Number edges that include v
+     * @return int degree number
+     */
+    int degree(Vertex v);
+
+    /** Return true if v is an endpoint of edge e.
+     * @param e edge to check
+     * @param v vertex to check
+     * @return true if they are incident, false if not
+     */
+    boolean areIncident(Edge e, Vertex v);
+
+    /** Return a list of all the vertices that can be reached from v,
+     * in the order in which they would be visited in a depth-first
+     * search starting at v.  
+     * @param v vertex to start the search from
+     * @return List of vertices found from v
+     */
+    List<Vertex> depthFirst(Vertex v);
+
+    /** Return a list of all the edges.
+     * @return List of all the edges in the graph
+     */
+    List<Edge> allEdges();
+
+    /** Return a list of all the vertices.
+     * @return Get list of all the vertices
+     */
+    List<Vertex> allVertices();
+
+}
