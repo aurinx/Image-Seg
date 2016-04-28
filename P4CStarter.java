da7a8faafc60e422150fe05576081a288e99afbb
SM 600.120 Sp16 <ipsm16@ugrad2.cs.jhu.edu>
2016-04-28 19:53:43 -0400

    checkstyle

--- P4CStarter.java
+++ P4CStarter.java
@@ -3,15 +3,16 @@ import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+/** Program that breaks picture into different parts.*/
+final class P4CStarter {
+    /** % pixels needed of image to print it.*/
+    static final double POINT5 = .05;
 
-
-public class P4CStarter{
+    /**For javadocs.*/
+    private P4CStarter() {
+    }
 
     /** Convert an image to a graph of Pixels with edges between
      *  north, south, east and west neighboring pixels.
@@ -19,17 +20,22 @@ public class P4CStarter{
      *  @param pd the distance object for pixels
      *  @return the graph that was created
      */
-    static WGraphP4<Pixel> imageToGraph(BufferedImage image, PixelDistance pd) { // input is image and istance function
-        WGraphP4<Pixel> g = new WGraphP4<Pixel>();//init a graph
-        ArrayList<ArrayList<GVertex<Pixel>>> plist= new ArrayList<ArrayList<GVertex<Pixel>>>(image.getWidth()); //make lists of list to represent 2d array . width is first array
-        for (int i =0; i < image.getWidth(); i++) { // initialize all columnn list
+
+
+    static WGraphP4<Pixel> imageToGraph(BufferedImage image, PixelDistance pd) {
+        WGraphP4<Pixel> g = new WGraphP4<Pixel>(); //init a graph
+        ArrayList<ArrayList<GVertex<Pixel>>> plist = new ArrayList<ArrayList
+            <GVertex<Pixel>>>(image.getWidth()); //make lists of list
+        for (int i = 0; i < image.getWidth(); i++) { //init all column list
             plist.add(new ArrayList<GVertex<Pixel>>(image.getHeight()));
         }
 //        System.out.println("Start: " + System.currentTimeMillis());
-        for (int i = 0; i < image.getWidth(); i++) { // make vertex with pixel for each pixel in image
+        // make vertex for each pixel in image
+        for (int i = 0; i < image.getWidth(); i++) {
             for (int j = 0; j < image.getHeight(); j++) {
-                Color mycolor = new Color(image.getRGB(i,j));
-                Pixel newpixel = new Pixel(i, j,mycolor.getRed(),mycolor.getGreen(),mycolor.getBlue(), mycolor); 
+                Color mycolor = new Color(image.getRGB(i, j));
+                Pixel newpixel = new Pixel(i, j, mycolor.getRed(),
+                    mycolor.getGreen(), mycolor.getBlue(), mycolor);
                 GVertex<Pixel> newv = new GVertex(newpixel, g.nextID());
                 g.addVertex(newv);
                 plist.get(i).add(newv);
@@ -38,10 +44,10 @@ public class P4CStarter{
 //        System.out.println("Start: " + System.currentTimeMillis());
 
         for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight() - 1 ; j++) {
+            for (int j = 0; j < image.getHeight() - 1; j++) {
                 GVertex<Pixel> a1 = plist.get(i).get(j);
-                GVertex<Pixel> a2 = plist.get(i).get(j+1);
-                g.addEdge(a1,a2,pd.distance(a1.data(), a2.data()));
+                GVertex<Pixel> a2 = plist.get(i).get(j + 1);
+                g.addEdge(a1, a2, pd.distance(a1.data(), a2.data()));
             }
         }
 //        System.out.println("Start: " + System.currentTimeMillis());
@@ -49,14 +55,29 @@ public class P4CStarter{
         for (int i = 0; i < image.getWidth() - 1; i++) {
             for (int j = 0; j < image.getHeight(); j++) {
                 GVertex<Pixel> a1 = plist.get(i).get(j);
-                GVertex<Pixel> a2 = plist.get(i+1).get(j);
-                g.addEdge(a1,a2,pd.distance(a1.data(), a2.data()));
+                GVertex<Pixel> a2 = plist.get(i + 1).get(j);
+                g.addEdge(a1, a2, pd.distance(a1.data(), a2.data()));
 
             }
         }
 
         return g;
 
+    }
+
+
+    /** Make an existing image all gray.
+      * @param image the input
+      * @return the output gray
+      */
+    private static BufferedImage makegray(BufferedImage image) {
+        final int gray = 0x202020;
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                image.setRGB(j, i, gray);
+            }
+        }
+        return image;
     }
 
 
@@ -67,62 +88,70 @@ public class P4CStarter{
      *  @return a list of the edges in the minimum spanning forest
      */
 
-    static List<WEdge<Pixel>> segmenter(WGraph<Pixel> g, double K) { // normal kruskals but adds that fuckign equation
-      WGraphP4<Pixel> newstuff = new WGraphP4();
-      List<GVertex<Pixel>> verti = g.allVertices();
-      List<WEdge<Pixel>> edges = g.allEdges();
-      Partition P = new Partition(verti.size()); 
-      PQHeap<WEdge<Pixel>> Q = new PQHeap<WEdge<Pixel>>();
+    private static List<WEdge<Pixel>> segmenter(WGraph<Pixel> g,
+        double kvalue) {
+        WGraphP4<Pixel> newstuff = new WGraphP4();
+        List<GVertex<Pixel>> verti = g.allVertices();
+        List<WEdge<Pixel>> edges = g.allEdges();
+        Partition newPartition = new Partition(verti.size()); 
+        PQHeap<WEdge<Pixel>> newQueue = new PQHeap<WEdge<Pixel>>();
 //      PriorityQueue<WEdge<Pixel>> Q = new PriorityQueue<WEdge<Pixel>>();
-      Q.init(edges);
+        newQueue.init(edges);
 
 //      for (WEdge<Pixel> temp7 : edges){ 
  //         Q.add(temp7);
  //     }
+        for (GVertex<Pixel> temp2 : verti) {
+            newstuff.addVertex(temp2);
+        }
 
-      for (GVertex<Pixel> temp2 : verti){
-          newstuff.addVertex(temp2);
-      }
+        ArrayList<maxmin> mmlist = new ArrayList<maxmin>(verti.size());
+        for (int i = 0; i < verti.size(); i++) { // add to mm to list
+            mmlist.add(i, new maxmin(verti.get(i)));
+        }
+        System.out.println(System.currentTimeMillis());
+        while (newQueue.size() > 0) {
+            WEdge<Pixel> temp = newQueue.peek();
+            newQueue.remove();
+            GVertex<Pixel> u = temp.source();
+            GVertex<Pixel> v = temp.end();
+            int a1 = newPartition.find(u.uniqueid());
+            int a2 = newPartition.find(v.uniqueid());
+            if (a1 != a2) {
+                maxmin mmu = mmlist.get(a1); // uses partition to get to root
+                maxmin mmv = mmlist.get(a2); // uses partition to get to root
+                if ((double) mmu.diffrc(mmv) <= ((double) Math.min(mmu.diffr(),
+                    mmv.diffr()) + (kvalue / (double) (mmu.getCount() + mmv.
+                    getCount())))) { //if red
+                    if ((double) mmu.diffgc(mmv) <= ((double) Math.min(
+                        mmu.diffg(),
+                        mmv.diffg()) + (kvalue / (double) (mmu.getCount()
+                        + mmv.getCount())))) { //if green
+                        if (((double) mmu.diffbc(mmv) <= (double)
+                            Math.min(mmu.diffb()
+                            , mmv.diffb()) + (kvalue / (double) (
+                            mmu.getCount() + mmv.getCount())))) { //if blue
+                            mmu.combine(mmv);
+                            newstuff.addEdge(temp);
+                            newPartition.union(u.uniqueid(),
+                                v.uniqueid()); // union them in partition
+                        }
+                    }
+                }
+            }
+        }
+      //System.out.println(System.currentTimeMillis());
 
-      ArrayList<maxmin> mmlist = new ArrayList<maxmin>(verti.size()); //max an arraylist of maxmin class according to unique id
-      for (int i = 0; i < verti.size(); i++){ // add to mm to list
-          mmlist.add(i, new maxmin(verti.get(i)));
-      }
-      System.out.println(System.currentTimeMillis());
-      while (Q.size() > 0) {
-          WEdge<Pixel> temp = Q.peek();
-          Q.remove();
-          GVertex<Pixel> u = temp.source();
-          GVertex<Pixel> v = temp.end();
-          int a1 = P.find(u.uniqueid());
-          int a2 = P.find(v.uniqueid());
-          if(a1 != a2){
-              maxmin mmu = mmlist.get(a1); // uses partition to get to root
-              maxmin mmv = mmlist.get(a2); // uses partition to get to root
-              if((double)mmu.diffrc(mmv) <= ((double)Math.min(mmu.diffr(), mmv.diffr()) + (K/(double)(mmu.getCount() + mmv.getCount())))){//if red
-                  if((double)mmu.diffgc(mmv) <= ((double)Math.min(mmu.diffg(), mmv.diffg()) + (K/(double)(mmu.getCount() + mmv.getCount())))){//if green
-                      if(((double)mmu.diffbc(mmv) <= (double)Math.min(mmu.diffb(), mmv.diffb()) + (K/(double)(mmu.getCount() + mmv.getCount())))){//if blue
-                          mmu.combine(mmv);//unions the two but really only edits the bigger root
-                          newstuff.addEdge(temp);
-
-                          P.union(u.uniqueid(),v.uniqueid()); // union them in partition
-                      }
-                  }
-              }
-
-          }
-      }
-      System.out.println(System.currentTimeMillis());
-
-      return newstuff.allEdges();
+        return newstuff.allEdges();
 
 
 
     }
-
+    /**The main function.
+      * @param args the input file name and k
+      */
     public static void main(String[] args) {
-    long StartTime = System.currentTimeMillis();
-        final int gray = 0x202020;
+        long startTime = System.currentTimeMillis();
 
         try {
           // the line that reads the image file
@@ -141,58 +170,47 @@ public class P4CStarter{
                              + (g.numVerts() - res.size()) + "\n");
 
             // make a background image to put a segment into
-            for (int i = 0; i < image.getHeight(); i++) {
-                for (int j = 0; j < image.getWidth(); j++) {
-                    image.setRGB(j, i, gray);
-                }
-            }
             WGraphP4<Pixel> h = new WGraphP4<Pixel>();
-            for(GVertex<Pixel> temp22: g.allVertices()){
+            for (GVertex<Pixel> temp22: g.allVertices()) {
                 h.addVertex(temp22);
             }
-            for (WEdge<Pixel> tempog : res){
+            for (WEdge<Pixel> tempog : res) {
                 h.addEdge(tempog);
             }
+            image = makegray(image);
+
             a = System.currentTimeMillis();
             System.out.println("After newgraph " + (a - diff));
             List<GVertex<Pixel>> og = h.allVertices();
-            int [] newarray = new int [og.size()];
-            for (int i = 0; i < newarray.length; i++)
+            int[] newarray = new int[og.size()];
+            for (int i = 0; i < newarray.length; i++) {
                 newarray[i] = 0;
+            }
             int z = 0;
-            for(GVertex<Pixel> temp : og){
-                if(newarray[temp.uniqueid()] == 0){
-                   List<GVertex<Pixel>> innerlist = h.depthFirst(temp);
-                   for(GVertex<Pixel> temp3 : innerlist){
-                      newarray[temp3.uniqueid()] =1;
-                   }
-                   if(innerlist.size() > (double)og.size()*.05){
-                       z++;
-                       for(GVertex<Pixel> i : innerlist) {
-                           Pixel d = i.data();
-                           image.setRGB(d.getx(), d.gety(), d.getrgb().getRGB()); // make it rgb
-                       }
-                       File f = new File("output" + z +".png");
-                       ImageIO.write(image,"png",f);
-                       for (int i = 0; i < image.getHeight(); i++) {
-                          for (int j = 0; j < image.getWidth(); j++) {
-                          image.setRGB(j, i, gray);
-                          }
-                       }
+            for (GVertex<Pixel> temp : og) {
+                if (newarray[temp.uniqueid()] == 0) {
+                    List<GVertex<Pixel>> innerlist = h.depthFirst(temp);
+                    for (GVertex<Pixel> temp3 : innerlist) {
+                        newarray[temp3.uniqueid()] = 1;
+                    }
+                    if (innerlist.size() > (double) og.size() * POINT5) {
+                        z++;
+                        for (GVertex<Pixel> i : innerlist) {
+                            Pixel d = i.data();
+                            image.setRGB(d.getx(), d.gety(), d.getrgb().
+                                getRGB());
+                        }
+                        File f = new File("output" + z + ".png");
+                        ImageIO.write(image, "png", f);
+                        image = makegray(image);
 
-                   }
-                } else {
+                    }
                 }
             }
-            diff = System.currentTimeMillis();
-            System.out.println("After out " + (diff - a));
-                System.out.println(z);
+            //diff = System.currentTimeMillis();
+            //System.out.println("After out " + (diff - a));
+            System.out.println(z);
 
-            // After you have a spanning tree connected component x, 
-            // you can generate an output image like this:
-            // You'll need to do that for each connected component,
-            // writing each one to a different file, clearing the
-            // image buffer first
 
         } catch (IOException e) {
             System.out.print("Missing File!\n");
@@ -203,3 +221,4 @@ public class P4CStarter{
     }
 
 }
+
